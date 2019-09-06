6f199442541f2a212995e9d4b3be903ff19a8885
Leila <nb.leila10@gmail.com>
2019-09-06 16:33:24 -0500

    Implement insert in max_heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,31 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        idx = len(self.storage)-1
+        while idx >= 1:
+            parentIdx = (idx-1)//2
+            parent = self.storage[parentIdx]
+            if value <= parent:
+                break
+            self.storage[parentIdx] = value
+            self.storage[idx] = parent
+            idx = parentIdx
+        return self.storage
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        pass
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        pass
